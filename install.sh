eb9d59e861d17dce481718b5211c73b37170383e
Andrii Korzh <Andrii.Korzh@gmail.com>
2016-04-22 14:11:35 +0300

    fix installer

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 cd /usr/local/Library/
-sudo curl -o RamDisk https://raw.githubusercontent.com/korzhyk/OSX-RAM-Disk/master/RamDisk
-sudo chmod 755 RamDisk
-sudo chgrp admin RamDisk
+curl -o RamDisk https://raw.githubusercontent.com/korzhyk/OSX-RAM-Disk/master/RamDisk
+chmod 755 RamDisk
 
 cd /Library/LaunchDaemons/
 sudo curl -o com.ramdisk.plist https://raw.githubusercontent.com/korzhyk/OSX-RAM-Disk/master/com.ramdisk.plist
