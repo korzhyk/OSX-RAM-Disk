5e343a0cd3540fa1f465dae7281dd9513c57e9e6
Andrii Korzh <Andrii.Korzh@gmail.com>
2016-04-22 20:10:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo mkdir -p /usr/local/bin
-curl -o /usr/local/bin/RamDisk https://raw.githubusercontent.com/korzhyk/OSX-RAM-Disk/master/RamDisk
-chmod 755 RamDisk
+sudo curl -o /usr/local/bin/RamDisk https://raw.githubusercontent.com/korzhyk/OSX-RAM-Disk/master/RamDisk
+sudo chmod 755 RamDisk
 
 cd /Library/LaunchDaemons/
 sudo curl -o com.ramdisk.plist https://raw.githubusercontent.com/korzhyk/OSX-RAM-Disk/master/com.ramdisk.plist
