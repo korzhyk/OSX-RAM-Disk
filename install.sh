1e280139c523121d12bc1236f8a8a7ad27b21b37
Andrii Korzh <Andrii.Korzh@gmail.com>
2017-03-06 01:04:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+echo "🚧 Make directory for binary file"
 sudo mkdir -p /usr/local/bin
+echo "🚚 Download binary"
 sudo curl -o /usr/local/bin/RamDisk https://raw.githubusercontent.com/korzhyk/OSX-RAM-Disk/master/RamDisk
 sudo chmod 755 /usr/local/bin/RamDisk
 
-cd /Library/LaunchAgents/
-sudo curl -o com.ramdisk.plist https://raw.githubusercontent.com/korzhyk/OSX-RAM-Disk/master/com.ramdisk.plist
-sudo chmod 644 com.ramdisk.plist
-sudo chgrp wheel com.ramdisk.plist
+echo "🚚 Download plist"
+sudo curl -o /Library/LaunchAgents/com.ramdisk.plist https://raw.githubusercontent.com/korzhyk/OSX-RAM-Disk/master/com.ramdisk.plist
+sudo chmod 644 /Library/LaunchAgents/com.ramdisk.plist
+sudo chgrp wheel /Library/LaunchAgents/com.ramdisk.plist
 
+echo "👌 Done"
 exit
